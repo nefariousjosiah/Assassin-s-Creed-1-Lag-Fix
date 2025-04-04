5ebe4e6d7287a5ec37d568e402fd5fbf410a2a7a
Josiah <148295938+nefariousjosiah@users.noreply.github.com>
2025-04-04 11:02:17 -0400

    updated bat file due to ubisoft server ip change

--- AC1LagFix.bat
+++ AC1LagFix.bat
@@ -7,14 +7,26 @@ if %errorLevel% neq 0 (
 )
 
 set hostsFile=%SystemRoot%\System32\drivers\etc\hosts
-set entry=203.132.26.155	localhost
+set entry1=203.132.26.155 localhost
+set entry2=216.98.50.240 localhost
 
-findstr /C:"%entry%" "%hostsFile%" >nul
+:: Check and add first entry
+findstr /C:"%entry1%" "%hostsFile%" >nul
 if %errorLevel% neq 0 (
-    echo %entry% >> "%hostsFile%"
-    echo Entry added successfully, you can now enjoy Assassin's Creed 1.
+    echo %entry1% >> "%hostsFile%"
+    echo Added: %entry1%
 ) else (
-    echo Entry already exists in the hosts file.
+    echo Entry already exists: %entry1%
 )
 
+:: Check and add second entry
+findstr /C:"%entry2%" "%hostsFile%" >nul
+if %errorLevel% neq 0 (
+    echo %entry2% >> "%hostsFile%"
+    echo Added: %entry2%
+) else (
+    echo Entry already exists: %entry2%
+)
+
+echo Entries updated successfully. You can now enjoy Assassin's Creed 1.
 pause
